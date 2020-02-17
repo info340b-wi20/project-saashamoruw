9722d94f2f687f9974d6810624aec48bdeaa640c
Saasha <saashm@uw.edu>
2020-02-17 15:14:07 -0800

    js fixes 1

--- js/index.js
+++ js/index.js
@@ -12,9 +12,9 @@ function fetchData() {
             return res.json();
         })
         .then(function (data) {
-            state.currProj = data.currentProj;
+            state.currProj = data.currProj;
             state.findProj = data.findProjects;
-            console.log(state);
+            renderShowcaseCards(state.currProj);
         });
 }
 fetchData();
@@ -24,38 +24,53 @@ fetchData();
 // Dynamically add data the user enters to the json file and creates its showcase card
 // Add code to make find projects cards
 // Sign in / sign up page js
+function renderShowcaseCards(obj) {
+    let sec = document.querySelector(".projects");
+    let row = document.createElement('div');
+    row.classList.add("row");
+    for(var i = 0; i < obj.length; i++) {
+        let card = createShowcaseCard(obj[i]);
+        row.appendChild(card);
+    }
+    sec.appendChild(row);
+}
 
 // Create one card that showcases projects with info and flip css
 function createShowcaseCard(obj) {
-    let sec = document.querySelector(".projects");
-    let row = document.createElement('div').classList.add("row");
-    let col = document.createElement('div').classList.add("col-sm-12 col-md-4");
-    let container = document.createElement('div').classList.add("cardContainer");
-    let content = document.createElement('div').classList.add("content-area");
+    let col = document.createElement('div');
+        col.classList.add("col-sm-12","col-md-4");
+    let container = document.createElement('div');
+        container.classList.add("cardContainer");
+    let content = document.createElement('div');
+        content.classList.add("content-area");
     let sideOne = createCardSideOne(obj);
     let sideTwo = createCardSideTwo(obj);
-    content.appendChild(sideOne).appendChild(sideTwo);
+    content.appendChild(sideOne, sideTwo);
     container.appendChild(content);
     col.appendChild(container);
-    row.appendChild(col);
-    sec.appendChild(row);
-    return sec;
+    return col;
 }
 
 // creates flip side of showcase card
 function createCardSideTwo(obj) {
-    let side = document.createElement('div').classList.add("side_one");
-    let card = document.createElement('div').classList.add("card");
-    let cardBody = document.createElement('div').classList.add("card-body");
+    let side = document.createElement('div');
+        side.classList.add("side_one");
+    let card = document.createElement('div');
+        card.classList.add("card");
+    let cardBody = document.createElement('div');
+        cardBody.classList.add("card-body");
      // Make footer with function
-     let footer = document.createElement('div').classList.add("card-footer");
+     let footer = document.createElement('div');
+        footer.classList.add("card-footer");
      let links = createLinkList(obj.links[0]);
      footer.appendChild(links);
     //Add title
-    let title = document.createElement('p').classList.add('card-title');
+    let title = document.createElement('p');
+        title.classList.add('card-title');
     title.textContent = obj.name;
     //Add Team Members
-    let team = document.createElement('p').classList.add('card-text');
+    let team = document.createElement('p');
+        team.classList.add('card-text');
     team.textContent = "Team Members: " + obj.team.join(', ');
     //Add skills
     let skills = createHighlighted(obj.skills);
@@ -63,23 +78,23 @@ function createCardSideTwo(obj) {
     let purpLink = document.createElement('a'); 
     purpLink.classList.add('highlight');
     purpLink.innerHTML = obj.purpose;
-    let purpose = document.createElement('p').classList.add('card-text').appendChild(purpLink);
+    let purpose = document.createElement('p');
+        purpose.classList.add('card-text');
+        purpose.appendChild(purpLink);
     //Link everything
-    cardBody.appendChild(title)
-            .appendChild(team)
-            .appendChild(skills)
-            .appendChild(purpose)
-            .appendChild(footer);
-    card.appendChild(card).appendChild(footer);
+    cardBody.appendChild(title, team, skills, purpose, footer);
+    card.appendChild(cardBody, footer);
     side.appendChild(card);
 }
 
 // Creates skills text with highlighted style
 function createHighlighted(obj) {
-    let skills = document.createElement('p').classList.add('card-text');
+    let skills = document.createElement('p');
+    skills.classList.add('card-text');
     skills.textContent = "Languages/Skills: ";
     for(let i = 0; i < obj.length; i++) {
-        let span = document.createElement('span').classList.add('highlight');
+        let span = document.createElement('span');
+        span.classList.add('highlight');
         span.textContent = obj[i];
         skills.appendChild(span);
     }
@@ -88,12 +103,17 @@ function createHighlighted(obj) {
 
 // Creates main side of showcase card
 function createCardSideOne(obj) {
-    let side = document.createElement('div').classList.add("side_one");
-    let card = document.createElement('div').classList.add("card");
-    let cardBody = document.createElement('div').classList.add("card-body");
-    let topImg = document.createElement('img').classList.add("card-img-top");
+    let side = document.createElement('div');
+        side.classList.add("side_one");
+    let card = document.createElement('div');
+    card.classList.add("card");
+    let cardBody = document.createElement('div');
+        cardBody.classList.add("card-body");
+    let topImg = document.createElement('img');
+        topImg.classList.add("card-img-top");
     // Make footer with function
-    let footer = document.createElement('div').classList.add("card-footer");
+    let footer = document.createElement('div');
+        footer.classList.add("card-footer");
     let links = createLinkList(obj.links[0]);
     footer.appendChild(links);
     // Adds link to image and adds image to card
@@ -101,14 +121,16 @@ function createCardSideOne(obj) {
     topImg.alt=obj.alt;
     card.innerHTML = topImg;
     //Add title
-    let title = document.createElement('p').classList.add('card-title');
+    let title = document.createElement('p');
+        title.classList.add('card-title');
     title.textContent = obj.name;
     //Add description
-    let text = document.createElement('p').classList.add('card-text');
+    let text = document.createElement('p');
+    text.classList.add('card-text');
     text.textContent = obj.description;
     // link everything
-    cardBody.appendChild(title).appendChild(text);
-    card.appendChild(cardBody).appendChild(footer);
+    cardBody.appendChild(title, text);
+    card.appendChild(cardBody, footer);
     side.appendChild(card);
     return side;
 }
@@ -116,16 +138,17 @@ function createCardSideOne(obj) {
 // Creates list of icon links for a particular showcase project
 
 function createLinkList(obj) {
-    let links = document.createElement('div').classList.add('links');
+    let links = document.createElement('div');
+        links.classList.add('links');
     let list = document.createElement('ul');
     for(var key in obj) {
         if(obj.key != "") {
             let curr = key.toString();
             let link = document.createElement('a');
-            link.href = obj.img;
+            link.href = "https://www." + curr + ".com";
             let icon = document.createElement('i');
-            icon.classList.add("fa fa-" + curr);
-            icon.setAttribute('aria-label', curr + 'logo');
+            icon.classList.add("fa", "fa-" + curr);
+            icon.setAttribute('aria-label', curr + ' logo');
             link.appendChild(icon);
             let li= document.createElement('li');
             li.appendChild(link);
