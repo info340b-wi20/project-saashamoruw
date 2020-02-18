6f50c330e6b89f6bb5bd412ffc197ffed0cdee6b
rajoshich <rajoshich@gmail.com>
2020-02-17 20:09:21 -0800

    form js and css

--- js/sign.js
+++ js/sign.js
@@ -1,2 +1,17 @@
 'use strict';
 
+
+
+function tabsChange(){
+let button = document.querySelector('.login');
+let field = document.querySelector('confirm');
+field.style.display = "none";
+document.getElementsByClassName('submit').textContent = "Log In!";
+document.querySelector('.msg').textContent = "Welcome Back!"
+button.classList.add('active');
+document.querySelector('signin').classList.remove('active');
+}
+
+
+
+
