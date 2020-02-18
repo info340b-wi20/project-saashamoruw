7222a49be3b260e9f13a3f8109360a57dccd8ee6
rajoshich <rajoshich@gmail.com>
2020-02-18 01:48:33 -0800

    sign in form works

--- js/sign.js
+++ js/sign.js
@@ -1,20 +1,42 @@
 'use strict';
-// Fix???
-function tabsChange(){
-let button = document.querySelector('.login');
-let field = document.querySelector('confirm');
-field.style.display = "none";
-document.getElementsByClassName('submit').textContent = "Log In!";
-document.querySelector('.msg').textContent = "Welcome Back!"
-button.classList.add('active');
-document.querySelector('signin').classList.remove('active');
-}
 
 let login = document.querySelector('#btnlogin');
 let signup = document.querySelector('#btnsignup');
 
-login.addEventListener('click', tabsChange('login'));
-signup.addEventListener('click', tabsChange('signup'));
+
+login.addEventListener('click', function(event) {
+event.preventDefault();
+    let field = document.querySelector('.confirm');      
+    field.style.display = "none";
+    let submit = document.querySelector('#button-submit');
+    submit.textContent = "Log In!"
+    let check = document.querySelector('.checkbox');
+    check.style.display = "none";    
+    let msg = document.querySelector('.msg');
+    msg.textContent = "Welcome Back!";
+    let change = document.querySelector('#btnsignup');
+    change.classList.add('active');   
+    let tabButton = document.querySelector('#btnlogin');
+    tabButton.classList.remove('active');
+});
+
+signup.addEventListener('click', function(event) {
+    event.preventDefault();
+    let field = document.querySelector('.confirm');
+    field.style.display = "inline-block";
+    let submit = document.querySelector('#button-submit');
+    submit.textContent = "Get Started!";
+    let check = document.querySelector('.checkbox');
+    check.style.display = "inline-block";
+    let msg = document.querySelector('.msg');
+    msg.textContent = "Create an Account!";
+    let change = document.querySelector('#btnlogin');
+    change.classList.add('active');
+    let tabButton = document.querySelector('#btnsignup');
+    tabButton.classList.remove('active');
+});
+
+
 
 
 
