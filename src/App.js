cc914fc0899312c8745fc63126769ecb5a77000b
Saasha <saashm@uw.edu>
2020-03-02 00:17:45 -0800

    App adjustments

--- src/App.js
+++ src/App.js
@@ -1,117 +1,20 @@
 import React, { Component } from 'react'; //import React Component
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, Row, 
-    CardFooter, CardDeck
-  } from 'reactstrap';
-  
- //  import 'css/style.css'
- // import _ from 'lodash'
+import {Projects}  from '../stage-2/src/App1';
+import {App2}  from '../stage-2/src/App2';
 
-
-// Takes in the data from properties
-export class App extends Component{
-    
-    constructor(cardsData) {
-        super(cardsData); 
-        this.state = {
-            cardsData : this.props.cardsData
-        };
-      }
-
-
-    render() {
-        this.cardsData = this.props.cardsData;
-        return (
-            <Row>
-                <CreateShowcaseCards cardsData={this.cardsData}/>
-            </Row>
-        )
+export class App extends Component {
+    constructor() {
+        constructor(cardsData) {
+            super(cardsData); 
+            this.state = {
+                cardsData : this.props.cardsData
+            };
+          }
+        
+          render() {
+            this.cardsData = this.props.cardsData;
+            <App2 cardsData={this.cardsData}/>
+            <Projects cards={this.cardsData}/>
+          }
     }
 }
-
-class CreateShowcaseCards extends Component {
-    render() {
-        this.cardsData = this.props.cardsData;
-        let cards = this.cardsData.map(function(oneCard) {
-            let currCard = (<CreateCard oneCardData = {oneCard} key={"card" + oneCard.name}/>);
-            return currCard;
-        });
-        return (
-            <CardDeck>
-            {cards}
-            </CardDeck>
-        );
-    }
-}
-
-class CreateCard extends Component {
-    render() {
-        this.cardData = this.props.oneCardData;
-        let card = (
-            <div className="col-sm-12 col-md-4">
-                <div className="cardContainer">
-                    <div className="content-area">
-                        <div className="side_one">
-                            <CreateSideOne cardData={this.cardData} key={"sideone" + this.cardData.name}/>
-                        </div>
-                        <div className="side_two">
-                        <CreateSideTwo cardData={this.cardData} key={"sidetwo" + this.cardData.name}/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-        return card;
-    }
-
-}
-
-class CreateSideOne extends Component {
-    render() {
-        this.cardData = this.props.cardData;
-        let sideOne = (
-            <Card key={this.cardData.name}>
-                <CardImg top width="100%" src={this.cardData.img} alt={this.cardData.alt} />
-                <CardBody>
-                    <CardTitle>{this.cardData.name} </CardTitle>
-                    <CardText>{this.cardData.description}</CardText>
-                </CardBody>
-                <CardFooter>
-                    <div className="links">
-                        <ul>
-                            <a href={this.cardData.links[0].github}><FontAwesomeIcon icon={['fab', 'github']} /></a>
-                        </ul>
-                    </div>
-                </CardFooter>
-            </Card>
-        )
-        return sideOne;
-    }
-}
-class CreateSideTwo extends Component {
-    render() {
-        this.cardData = this.props.cardData;
-        let sideTwo = (
-            <Card>
-                <CardBody>
-                    <CardTitle>{this.cardData.name}</CardTitle>
-                    <CardText>{"Team Members: " + this.cardData.team}</CardText>
-                    <CardText><span className="highlighted">{"Skills/Languages" + this.cardData.skills}</span></CardText>
-                    <CardText>{"Purpose: " + this.cardData.purpose}</CardText>
-                </CardBody>
-                <CardFooter>
-                    <div className="links">
-                        <ul>
-                        <a href={this.cardData.links[0].github}><FontAwesomeIcon icon={['fab', 'github']} /></a>
-                        </ul>
-                    </div>
-                    </CardFooter>
-            </Card>
-        )
-        return sideTwo;
-    }
-}
-export default App;
--- stage-2/src/App1.js
+++ stage-2/src/App1.js
@@ -1,73 +1,79 @@
 import React, { Component } from 'react'; //import React Component
-import LinkIcon from '@material-ui/icons/Link';
-import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import {
     Card, CardImg, CardText, CardBody,
     CardTitle, Row, 
     CardFooter, CardDeck
   } from 'reactstrap';
+  
+ //  import 'css/style.css'
+ // import _ from 'lodash'
 
-export class App extends Component {
-    render() {
-        this.cardsData = this.props.cardsData;
-        return <createCardLayout cardsData = {this.cardsData} />
-    }
-}
+
 // Takes in the data from properties
-class createCardLayout extends Component{
+export class Projects extends Component{
+    
+    constructor(cardsData) {
+        super(cardsData); 
+        this.state = {
+            cardsData : this.props.cardsData
+        };
+      }
+
+
     render() {
         this.cardsData = this.props.cardsData;
         return (
             <Row>
-                <createShowcaseCards cardsData={this.cardsData}/>
+                <CreateShowcaseCards cardsData={this.cardsData}/>
             </Row>
         )
     }
 }
 
-class createShowcaseCards extends Component {
+class CreateShowcaseCards extends Component {
     render() {
         this.cardsData = this.props.cardsData;
-        let cards = this.cardsData.map((card) => {
-            let oneCard = <createCard oneCardData = {card}/>
-            return oneCard;
+        let cards = this.cardsData.map(function(oneCard) {
+            let currCard = (<CreateCard oneCardData = {oneCard} key={"card" + oneCard.name}/>);
+            return currCard;
         });
         return (
             <CardDeck>
-                {cards}
+            {cards}
             </CardDeck>
-        )
+        );
     }
 }
 
-class createCard extends Component {
+class CreateCard extends Component {
     render() {
-        this.cardData = this.props.cardData;
+        this.cardData = this.props.oneCardData;
         let card = (
             <div className="col-sm-12 col-md-4">
                 <div className="cardContainer">
                     <div className="content-area">
                         <div className="side_one">
-                            <createSideOne cardData={this.cardData}/>
+                            <CreateSideOne cardData={this.cardData} key={"sideone" + this.cardData.name}/>
                         </div>
                         <div className="side_two">
-                            <createSideTwo cardData={this.cardData}/>
+                        <CreateSideTwo cardData={this.cardData} key={"sidetwo" + this.cardData.name}/>
                         </div>
                     </div>
                 </div>
             </div>
-        )
-        return {card};
+        );
+        return card;
     }
 
 }
 
-class createSideOne extends Component {
+class CreateSideOne extends Component {
     render() {
         this.cardData = this.props.cardData;
         let sideOne = (
-            <Card>
+            <Card key={this.cardData.name}>
                 <CardImg top width="100%" src={this.cardData.img} alt={this.cardData.alt} />
                 <CardBody>
                     <CardTitle>{this.cardData.name} </CardTitle>
@@ -76,38 +82,35 @@ class createSideOne extends Component {
                 <CardFooter>
                     <div className="links">
                         <ul>
-                            <li><Link to ={this.cardData.links[0].github}><FontAwesomeIcon icon="github" /></Link></li>
-                            <li><Link to="saasha05.github.io"> <LinkIcon/> </Link> </li>
+                            <a href={this.cardData.links[0].github}><FontAwesomeIcon icon={['fab', 'github']} /></a>
                         </ul>
                     </div>
                 </CardFooter>
             </Card>
         )
-        return {sideOne};
+        return sideOne;
     }
 }
-
-class createSideTwo extends Component {
+class CreateSideTwo extends Component {
     render() {
         this.cardData = this.props.cardData;
-        let sideOne = (
+        let sideTwo = (
             <Card>
                 <CardBody>
                     <CardTitle>{this.cardData.name}</CardTitle>
-                    <CardText>{"Team Members:" + this.cardData.team}</CardText>
+                    <CardText>{"Team Members: " + this.cardData.team}</CardText>
                     <CardText><span className="highlighted">{"Skills/Languages" + this.cardData.skills}</span></CardText>
                     <CardText>{"Purpose: " + this.cardData.purpose}</CardText>
                 </CardBody>
                 <CardFooter>
                     <div className="links">
                         <ul>
-                            <li><Link to={this.cardData.links[0].github}> <FontAwesomeIcon icon="github" /> </Link> </li>
-                            <li><Link to="google.com"> <LinkIcon/> </Link> </li>
+                        <a href={this.cardData.links[0].github}><FontAwesomeIcon icon={['fab', 'github']} /></a>
                         </ul>
                     </div>
                     </CardFooter>
             </Card>
         )
-        return {sideOne};
+        return sideTwo;
     }
 }
