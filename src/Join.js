4a7685e006d93a186365823d3371249b0bae0c94
rajoshich <rajoshich@gmail.com>
2020-03-02 17:55:14 -0800

    Message form doesnt crash!

--- src/Join.js
+++ src/Join.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'; //import React Component
 import _ from 'lodash'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import {  Card, CardImg, CardText, CardBody,CardTitle, Row, Col, CardFooter, CardDeck } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Row, Col, CardFooter, CardDeck } from 'reactstrap';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -12,22 +12,24 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
 import './index.css';
+import { Typography } from '@material-ui/core';
 
 
-// Find Projects page 
-export class Join extends Component { 
+// Join Projects page 
+export class Join extends Component {
 
-constructor(cards) {
-    super(cards);
-    this.cardsData = this.props.cards;
-}
-    
-render() { 
-    return (
-        <Row>
-            <CreateShowcaseCards cardsData={this.cardsData}/>
-        </Row>
-)}
+    constructor(cards) {
+        super(cards);
+        this.cardsData = this.props.cards;
+    }
+
+    render() {
+        return (
+            <Row>
+                <CreateShowcaseCards cardsData={this.cardsData} />
+            </Row>
+        )
+    }
 }
 
 
@@ -35,12 +37,12 @@ render() {
 class CreateShowcaseCards extends Component {
     render() {
         this.cardsData = this.props.cardsData;
-        let cards = this.cardsData.map(function(oneCard) {
-            let currCard = (<CreateCard oneCardData = {oneCard} key={"card" + oneCard.name}/>);
+        let cards = this.cardsData.map(function (oneCard) {
+            let currCard = (<CreateCard oneCardData={oneCard} key={"card" + oneCard.name} />);
             return currCard;
         });
         return (
-           cards
+            cards
         );
     }
 }
@@ -49,14 +51,14 @@ class CreateCard extends Component {
     render() {
         this.cardData = this.props.oneCardData;
         let card = (
-            <Col className ="col">
+            <Col className="col">
                 <div className="cardContainer">
                     <div className="content-area">
                         <div className="side_one">
-                            <CreateSideOne cardData={this.cardData} key={"sideone" + this.cardData.name}/>
+                            <CreateSideOne cardData={this.cardData} key={"sideone" + this.cardData.name} />
                         </div>
                         <div className="side_two">
-                        <CreateSideTwo cardData={this.cardData} key={"sidetwo" + this.cardData.name}/>
+                            <CreateSideTwo cardData={this.cardData} key={"sidetwo" + this.cardData.name} />
                         </div>
                     </div>
                 </div>
@@ -74,14 +76,11 @@ class CreateSideOne extends Component {
                 <CardImg top width="100%" src={this.cardData.img} alt={this.cardData.alt} />
                 <CardBody>
                     <CardTitle className="cardTitle">{this.cardData.name} </CardTitle>
-                    <CardText className = "cardText">{this.cardData.description}</CardText>
-                    
+                    <CardText className="cardText">{this.cardData.description}</CardText>
+
                 </CardBody>
 
-                <CardFooter>
-              <MessageButton/>
-              </ CardFooter>
-
+                <MessageButton />
             </Card>
         )
         return sideOne;
@@ -90,19 +89,19 @@ class CreateSideOne extends Component {
 class CreateSideTwo extends Component {
     render() {
         // shows up as objects
-      //  let skills = this.props.cardData.skills.map((i) => {      
-     //   let obj = <span className="highlight">{i}</span>;
-     //   return obj;
-     //   });
+        //  let skills = this.props.cardData.skills.map((i) => {      
+        //   let obj = <span className="highlight">{i}</span>;
+        //   return obj;
+        //   });
 
-        this.cardData = this.props.cardData;    
+        this.cardData = this.props.cardData;
 
         let sideTwo = (
             <Card className="card" key={this.cardData.name}>
                 <CardBody>
                     <CardTitle>{this.cardData.name}</CardTitle>
                     <CardText><span className="highlight">{"Skills/Languages" + this.cardData.skills}</span></CardText>
-                    <CardText>{"Positions Open: " + this.cardData.positions}</CardText>
+                    <CardText>{"Positions Open: " + this.cardData.position}</CardText>
                     <CardText>{"Members Needed: " + this.cardData.needed}</CardText>
                     <CardText>{"Duration: " + this.cardData.duration}</CardText>
                     <CardText>{"Start Date: " + this.cardData.start}</CardText>
@@ -111,9 +110,8 @@ class CreateSideTwo extends Component {
                     <CardText>{"Experience Level: " + this.cardData.exp}</CardText>
                 </CardBody>
 
-              <CardFooter>
-              <MessageButton/>
-              </ CardFooter>
+
+                <MessageButton />
 
             </ Card>
         )
@@ -121,105 +119,155 @@ class CreateSideTwo extends Component {
     }
 }
 
+// Form to message group!
 
+class MessageButton extends Component {
+    // this.state = {};
+    constructor(props) {
+        super(props);
+        this.state = {
+            openDialog: '',
+            text: 'Request to Join!'
+        };
 
-// Opens a pop up message box when the join project button is clicked
-// Documentation from https://material-ui.com/components/dialogs/#form-dialogs followed
-export class MessageButton extends Component {
+        this.handleOpenDialog = this.handleOpenDialog.bind(this);
+        this.handleCloseDialog = this.handleCloseDialog.bind(this);
+        this.handleSubmitDialog = this.handleSubmitDialog.bind(this);
+
+    }
+
+    handleOpenDialog() {
+        this.setState({ openDialog: true });
+    }
+
+    handleCloseDialog() {
+        this.setState({
+            openDialog: false
+        });
+        alert('Your message was discarded.');
+    }
+
+    handleSubmitDialog() {
+        this.setState({
+            openDialog: false,
+            text: 'Requested.'
+        });
+        alert('Your message to join the project has been sent!');
+    }
+
     render() {
-         function FormDialog() {
-            const [open, setOpen] = React.useState(false);
-         
-          
-            const handleClickOpen = () => {
-              setOpen(true);
-            };
-          
-            const handleClose = () => {
-              setOpen(false);
-            };
-
-
-            return (
+        return (
+            <CardFooter>
                 <div className="submit-button">
-                 <Button className = "btn btn-dark" onClick={handleClickOpen}>Request to Join!</Button>
-            
-                  <Dialog open={open} onClose={handleClose} aria-label="form-dialog-title">
+                    <label for="submitbutton" aria-label="submit button"></label>
+                    <button id="button-submit" type="submit" className="btn btn-dark submit" onClick={this.handleOpenDialog}>{this.state.text}</button></ div>
+                <Dialog open={this.state.openDialog} onClose={this.handleCloseDialog} aria-label="form-dialog-title">
                     <DialogTitle id="form-dialog-title">Request to Join: Message Project Group</DialogTitle>
                     <DialogContent>
-                      <DialogContentText>To join this project, please fill out the following fields.</DialogContentText>
-                      <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Name"
-                        type="name"
-                        fullWidth
-                      />
+                        <DialogContentText>To join this project, please fill out the following fields.</DialogContentText>
                         <TextField
-                        autoFocus
-                        margin="dense"
-                        id="email"
-                        label="Email Address"
-                        type="email"
-                        fullWidth
-                      />
+                            autoFocus
+                            margin="dense"
+                            placeholder="Enter Full Name"
+                            id="name"
+                            label="Name"
+                            type="name"
+                            fullWidth
+                            required="true"
+                        />
                         <TextField
-                        autoFocus
-                        margin="dense"
-                        id="letter"
-                        label="Cover Letter"
-                        type="letter"
-                        fullWidth
-                      />
+                            autoFocus
+                            margin="dense"
+                            id="emailid"
+                            label="Email ID"
+                            placeholder="Enter Email ID"
+                            type="letter"
+                            fullWidth
+                            required="true"
+                        />
+
+                        <TextField
+                            margin="dense"
+                            id="availability"
+                            label="Availability"
+                            placeholder="When are you available to work from? How many hours per week?"
+                            type="input"
+                            required="true"
+                            fullWidth
+                        />
+                        <TextField
+                            margin="dense"
+                            id="contact"
+                            label="Contact Information"
+                            placeholder="How can we reach you?"
+                            type="input"
+                            fullWidth
+                        />
+                        <Typography>Upload Resume:</ Typography>
+                        <TextField
+                            margin="dense"
+                            id="resume"
+                            aria-label="Attach Resume"
+                            type="file"
+                            fullWidth
+                            required="true"
+                        />
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="letter"
+                            label="Cover Letter"
+                            placeholder="Why do you want to join this project?"
+                            type="letter"
+                            multiline="true"
+                            fullWidth
+                        />
                     </DialogContent>
                     <DialogActions>
-                      <Button onClick={handleClose} color="primary">Cancel</Button>
-                      <Button onClick={handleClose} color="primary">Send</Button>
+                        <Button onClick={this.handleCloseDialog} color="primary">Cancel</Button>
+                        <Button onClick={this.handleSubmitDialog} color="primary">Send</Button>
                     </DialogActions>
-                  </Dialog>
-                </div>
-
-            );
+                </Dialog>
+            </CardFooter>
+        );
     }
-    return (FormDialog)
 }
 
-}
+
 
 
 export default Join;
 
 
 
- /*
- 
+/*
+
 // Banner Text
 class Banner extends Component {
-    render() {
-        return (
-           <div className="banner-text">
-                <h1>Discover Opportunities</h1>
-                <p>Find projects to work on and expore your passion!</p>
-            </div>
-        )
-    }
+   render() {
+       return (
+          <div className="banner-text">
+               <h1>Discover Opportunities</h1>
+               <p>Find projects to work on and expore your passion!</p>
+           </div>
+       )
+   }
 }
 
 
 // search bar function if time?
 // Search Bar on pages
-class SearchBar extends Component { 
-    render() {
-        return (
-            <div className="search-bar" role="search" aria-label="search bar"> 
-            <input type="text" placeholder="Search for position..."></input>
-            <button className = "btn btn-info">
-                <i className="fa fa-search fa-2x" aria-label="search icon"></i>
-            </button>
-        </div>
-        )
-    }
+class SearchBar extends Component {
+   render() {
+       return (
+           <div className="search-bar" role="search" aria-label="search bar">
+           <input type="text" placeholder="Search for position..."></input>
+           <button className = "btn btn-info">
+               <i className="fa fa-search fa-2x" aria-label="search icon"></i>
+           </button>
+       </div>
+       )
+   }
 }
 
 
@@ -231,18 +279,18 @@ class SearchBar extends Component {
 
 
 
-           <div className="side_two">
-         <div className="card">
-            <div className="card-body">
-               <p className="card-title"> {this.props.card.name} </p>
-               <p className="card-text"> {this.props.card.description} </p>
-               <p className="card-text">Languages/Skills: {skill} </p>
-               <p className="card-text">Positions Open: {positions} </p>
-               <p className="card-text">Members Needed: {this.props.card.members-needed} </p>
-               <p className="card-text">Duration: {this.props.card.duration} </p>
-               <p className="card-text">Start Date: {this.props.card.start} </p>
-               <p className="card-text">Time Commitment: {this.props.card.time} </p>
-               <p className="card-text">Experience Level: {this.props.card.exp-level} </p>
-               <p className="card-text"><a className="highlight"> {this.props.card.purpose} </a></p>
-            </div>
- */
+          <div className="side_two">
+        <div className="card">
+           <div className="card-body">
+              <p className="card-title"> {this.props.card.name} </p>
+              <p className="card-text"> {this.props.card.description} </p>
+              <p className="card-text">Languages/Skills: {skill} </p>
+              <p className="card-text">Positions Open: {positions} </p>
+              <p className="card-text">Members Needed: {this.props.card.members-needed} </p>
+              <p className="card-text">Duration: {this.props.card.duration} </p>
+              <p className="card-text">Start Date: {this.props.card.start} </p>
+              <p className="card-text">Time Commitment: {this.props.card.time} </p>
+              <p className="card-text">Experience Level: {this.props.card.exp-level} </p>
+              <p className="card-text"><a className="highlight"> {this.props.card.purpose} </a></p>
+           </div>
+*/
--- src/Sign.js
+++ src/Sign.js
@@ -8,31 +8,32 @@ import _ from 'lodash';
 // https://material-ui.com/components/dialogs/ use for log in
 
 // Sign In page 
-export class Sign extends Component { 
-  
-render() { 
-return(
+export class Sign extends Component {
 
-            <TabContent />
+  render() {
+    return (
 
-)}
+      <TabContent />
+
+    )
+  }
 }
 
 //////////////
 
 // Tab contents for sign in or log in
 class TabContent extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-    email: '',
-    password: '',
-    confirm: '',
-  };
+      email: '',
+      password: '',
+      confirm: '',
+    };
 
-  this.handleChange = this.handleChange.bind(this);
-  this.handleSubmit = this.handleSubmit.bind(this);
-}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
   handleChange(event) {
     this.setState({
@@ -43,55 +44,55 @@ class TabContent extends Component {
   handleSubmit(event) {
     event.preventDefault();
     let input = this.state;
-    console.log(input);
-    
-    if(input.password != input.confirm) {
+
+    if (input.password != input.confirm) {
       alert('Passwords do not match.');
     } else {
-    alert( this.state.email + ', welcome back! You are signed in!');
+      alert(this.state.email + ', welcome back! You are signed in!');
+
     }
   }
 
-    render() {
+  render() {
 
-        return(
-            <form className="tab-content" onSubmit = {this.handleSubmit}> 
-                 <ul className ="tabs">
-                <li className="tab-button"><button id="btnsignup" className ="btn btn-info tablinks active">Sign Up</button></li>
-                <li className="tab-button"><button id="btnlogin" className ="btn btn-info tablinks">Log In</button></li>
-               </ul>
-                  <div><h1 className = "msg">Create an Account!</h1></div> 
+    return (
+      <form className="tab-content" onSubmit={this.handleSubmit}>
+        <ul className="tabs">
+          <li className="tab-button"><button id="btnsignup" className="btn btn-info tablinks active">Sign Up</button></li>
+          <li className="tab-button"><button id="btnlogin" className="btn btn-info tablinks">Log In</button></li>
+        </ul>
+        <div><h1 className="msg">Create an Account!</h1></div>
 
-                    <div className = "form-input">
-                    <label for="emailid"><b>Email ID</b></label> 
-                    <input type="text" for = "email" id = "email" name="email" aria-label="input email" placeholder="Enter Email ID" minLength={6} required onChange={this.handleChange} /></div> 
-                    
-                    <div className = "form-input">
-                    <label for="password"><b>Password</b></label>  
-                    <input type="text" for = "password" id = "password" name="password"  aria-label="input password" placeholder="Enter Password" minLength={6} required onChange={this.handleChange}/></div>
-                    
-                    <div className = "form-input">
-                    <label for="confirm-password"><b>Confirm Password</b></label> 
-                    <input type="text" for = "confirm" id = "confirm" name="confirm"  aria-label="confirm password" placeholder="Re-enter Password" minLength={6} required onChange={this.handleChange} /></div>   
-                    <br/>
+        <div className="form-input">
+          <label for="emailid"><b>Email ID</b></label>
+          <input type="text" for="email" id="email" name="email" aria-label="input email" placeholder="Enter Email ID" minLength={6} required onChange={this.handleChange} /></div>
 
-                    <div className="form-input">
-                        <input type="checkbox" className="form-check-input" required />
-                        <label className="form-check-label" for="check">Agree to Terms and Conditions.</label>
-                        </div>  
-                        <br/>
+        <div className="form-input">
+          <label for="password"><b>Password</b></label>
+          <input type="text" for="password" id="password" name="password" aria-label="input password" placeholder="Enter Password" minLength={6} required onChange={this.handleChange} /></div>
 
-                        <div className = "submit-button">
-                        <label for="submitbutton" aria-label="submit button"></label>
-                        <button id = "button-submit" type="submit" className = "btn btn-dark submit">Get Started!</button>
-                        </div>  
-               </form>
+        <div className="form-input">
+          <label for="confirm-password"><b>Confirm Password</b></label>
+          <input type="text" for="confirm" id="confirm" name="confirm" aria-label="confirm password" placeholder="Re-enter Password" minLength={6} required onChange={this.handleChange} /></div>
+        <br />
 
-        )
-    }
+        <div className="form-input">
+          <input type="checkbox" className="form-check-input" required />
+          <label className="form-check-label" for="check">Agree to Terms and Conditions.</label>
+        </div>
+        <br />
+
+        <div className="submit-button">
+          <label for="submitbutton" aria-label="submit button"></label>
+          <button id="button-submit" type="submit" className="btn btn-dark submit">Get Started!</button>
+        </div>
+      </form>
+
+    )
+  }
 }
 
-    
+
 export default Sign;
 
 // https://css-tricks.com/video-screencasts/147-starting-react-powered-comment-form/
