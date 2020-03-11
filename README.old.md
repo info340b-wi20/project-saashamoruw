48d369c256fb41a6d165539552fa1417f4195fdb
rajoshich <rajoshich@gmail.com>
2020-03-11 02:52:08 -0700

    deployed to firebase

--- README.old.md
+++
@@ -1,5 +0,0 @@
-# Course Project
-
-This repository contains code for an interactive web app, created for the _Client-Side Web Development_ course at the UW iSchool by Saasha Mor (saashm@uw.edu) and Rajoshi Chakravarty (rajoshi@uw.edu)
-
-The site can be viewed at https://info340b-wi20.github.io/project-saashamoruw/index.html
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase/app';
+
 // names and descriptions for Join.json taken taken from kickstarter.com
  import CARDS from './cards.js'; 
 //import CARDS from './Join.json'; 
@@ -13,3 +15,19 @@ ReactDOM.render(<App cardsData={CARDS} />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+// paste below body before using firebase s
+/*
+
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/7.10.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/7.10.0/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
+
+ */
